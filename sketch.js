3bc56af76072b07ab26a3f69a38294f415434674
Tanjeer63 <75249275+Tanjeer63@users.noreply.github.com>
2021-02-15 16:28:14 +0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,35 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine;
+var world;
+var ground;
+
+function setup(){
+  createCanvas(3000,800);
+  engine = Engine.create();
+  world = engine.world;
+  
+  ground = new Ground(600,600,1200,20);
+
+
+
+
+
+
+
+
+    
+}
+
+function draw(){
+  background(255);   
+    
+  ground.display();
+
+
+  
+}
+
